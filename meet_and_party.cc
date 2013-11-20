fe3f873603e8b5893c6bbd812d45459de7c89ac9
washingtonzju <zhongkai.mi@gmail.com>
2013-11-20 19:47:20 +0800

    Update meet_and_party.cc

--- meet_and_party.cc
+++ meet_and_party.cc
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include <cmath>
 #include <vector>
 #include <utility>
@@ -6,89 +7,100 @@
 
 using namespace std;
 
-bool comp(const pair<int, int> &a, const pair<int, int> &b)
+long long calculateDis(int ax, int ay, int bx, int by)
 {
-    if(a.first == b.first) return a.second < b.second;
-    return a.first < b.first;
+    return (long long)(abs(bx - ax) + abs(by - ay));
 }
 
-void calculateCenter(vector<vector<int> > &region, double &ax, double &ay, long long &weight_sum)
+long long calSum(vector<vector<int> > &region, int x, int y)
 {
-    for(int i=0;i<region.size();++i)
+    long long sum = 0L;
+    for(int j=0;j<region.size();++j)
     {
-        long long temp_weight = (region[i][2] - region[i][0] + 1)*(region[i][3] - region[i][1] +1);
-        ax = temp_weight*(region[i][0]+region[i][2])/2;
-        ay = temp_weight*(region[i][1]+region[i][3])/2;
-        weight_sum += temp_weight;
-    }
-    ax /= weight_sum;
-    ay /= weight_sum;
-}
-
-double calculateDis(double ax, double ay, double bx, double by)
-{
-    return (ax - bx)*(ax - bx) + (ay - by)*(ay - by);
-}
-
-void findNearestInRegion(double ax, double ay, int &temp_x, int &temp_y, vector<int> &square)
-{
-    if( ax >= square[0] && ax <= square[2])
-    {
-        temp_x = floor(ax + 0.5);
-    }else if( ax < square[0]) {
-        temp_x = square[0];
-    } else{
-        temp_x = square[2];
-    }
-
-    if( ay >= square[1] && ay <= square[3])
-    {
-        temp_y = floor(ay + 0.5);
-        
-    }else if( ay < square[1]) {
-        temp_y = square[1];
-    } else{
-        temp_y = square[3];
-    }
-}
-
-void findPosition(vector<vector<int> > &region, int &x, int &y, int &dis)
-{
-    double ava_x=0.0, ava_y=0.0;
-    long long weight_sum = 0;
-    calculateCenter(region, ava_x, ava_y, weight_sum);
-
-    double delta = 1000000000000000000.0;
-    vector<pair<int, int> > cands;
-    
-    for(int i=0;i<region.size();++i)
-    {
-        int temp_x, temp_y;
-        findNearestInRegion(ava_x, ava_y, temp_x, temp_y, region[i]);
-        cands.push_back(pair<int, int>(temp_x, temp_y));
-    }
-    sort(cands.begin(), cands.end());
-    for(int i=0;i<region.size();++i)
-    {
-        double temp_dis = calculateDis(cands[i].first, cands[i].second, ava_x, ava_y);
-        if(temp_dis < delta)
-        {      
-            x = cands[i].first;
-            y = cands[i].second;
-            delta = temp_dis;
+        for(int l=region[j][0];l<=region[j][2];++l)
+        {
+            for(int m=region[j][1];m<=region[j][3];++m)
+            {
+                long long temp = calculateDis(l, m, x, y);
+                sum+=temp;
+            }
         }
     }
-    dis = (int)(sqrt(delta));
+    return sum;
+}
+
+void findPosition(vector<vector<int> > &region, int &x, int &y, long long &dis)
+{
+    double temp_x=0, temp_y=0;
+    long long weight_sum = 0;
+
+    for(int i=0;i<region.size();++i)
+    {
+        temp_x = (region[i][2]+region[i][0])/2.0;
+        temp_y = (region[i][3]+region[i][1])/2.0;
+        weight_sum += (region[i][2]-region[i][0]+1)*(region[i][3]-region[i][1]+1);
+    }
+
+    temp_x/=weight_sum;
+    temp_y/=weight_sum;
+
+    printf("%lf ------- %lf\n", temp_x, temp_y);
+    vector<pair<int, int> > cands;
+
+    for(int i=0;i<1;++i)
+        for (int j=0;j<1;++j)
+        {
+            int intx = (i%2==0)?floor(temp_x):ceil(temp_x);
+            int inty = (j%2==0)?floor(temp_y):ceil(temp_y);
+            cands.push_back(pair<int, int>(intx, inty));
+        }
+
+    vector<pair<int, int> > res;
+
+    for(int i=0;i<cands.size();++i)
+    {
+        int fit_x, fit_y;
+        long long dd = 100000000000000L;;
+        for(int j=0;j<region.size();++j)
+        {
+            for(int l=region[j][0];l<=region[j][2];++l)
+            {
+                for(int m=region[j][1];m<=region[j][3];++m)
+                {
+                    long long temp = calculateDis(l,m,cands[i].first, cands[i].second);
+                    if(temp < dd)
+                    {
+                        dd = temp;
+                        fit_x = l;
+                        fit_y = m;
+                    }
+                }
+            }
+        }
+        res.push_back(pair<int, int>(fit_x, fit_y));        
+    }
+
+    weight_sum = 1000000000000L;
+    for(int i=0;i<res.size();++i)
+    {
+        long long temp_sum = calSum(region, res[i].first, res[i].second);
+        if(temp_sum < weight_sum){
+            weight_sum = temp_sum;
+            x = res[i].first;
+            y = res[i].second;
+        }
+    }
 }
 
 int main()
 {
     int T, B;
-    int x, y, dis;
+    int x, y;
+    long long dis;
     vector<vector<int> > region;
     scanf("%d", &T);
 
-    for(int i=1;i<=T;++i)
+    for(int ic=1;ic<=T;++ic)
     {
         scanf("%d", &B);
         region.clear();
@@ -100,7 +112,7 @@ int main()
             region.push_back(tmp);            
         }
         findPosition(region, x, y, dis);
-        printf("Case #%d: %d %d %d\n", i, x, y, dis);
+        printf("Case #%d: %d %d %lld\n", ic, x, y, dis);
     }
     return 0;
 }
