39a6e6bd6e8a44816dd44e1b25a6710a1531bb87
washingtonzju <zhongkai.mi@gmail.com>
2013-11-20 22:52:19 +0800

    Update meet_and_party.cc

--- meet_and_party.cc
+++ meet_and_party.cc
@@ -6,90 +6,68 @@
 #include <algorithm>
 
 using namespace std;
+typedef pair<int, int> pnt;
+
+bool comp(const pnt &a, const pnt &b)
+{
+    if(a.first==b.first)
+        return a.second < b.second;
+    else
+        return a.first < b.first;
+}
 
 long long calculateDis(int ax, int ay, int bx, int by)
 {
     return (long long)(abs(bx - ax) + abs(by - ay));
 }
 
-long long calSum(vector<vector<int> > &region, int x, int y)
+double tinyDis(double ax, double ay, double bx, double by)
+{
+    return fabs(bx - ax) + fabs(by - ay);
+}
+
+long long calSum(vector<pnt > &region, int x, int y)
 {
     long long sum = 0L;
-    for(int j=0;j<region.size();++j)
-    {
-        for(int l=region[j][0];l<=region[j][2];++l)
-        {
-            for(int m=region[j][1];m<=region[j][3];++m)
-            {
-                long long temp = calculateDis(l, m, x, y);
-                sum+=temp;
-            }
-        }
-    }
+    for(int i=0;i<region.size();++i)
+        sum+=calculateDis(region[i].first, region[i].second, x, y);
     return sum;
 }
 
-void findPosition(vector<vector<int> > &region, int &x, int &y, long long &dis)
+void findPosition(vector<pnt > &region, int &x, int &y, long long &dis)
 {
+    if(region.size()==1){
+        x=region[0].first;
+        y=region[0].second;
+        dis = 0L;
+        return;        
+    }
+    
     double temp_x=0, temp_y=0;
-    long long weight_sum = 0;
-
     for(int i=0;i<region.size();++i)
     {
-        temp_x = (region[i][2]+region[i][0])/2.0;
-        temp_y = (region[i][3]+region[i][1])/2.0;
-        weight_sum += (region[i][2]-region[i][0]+1)*(region[i][3]-region[i][1]+1);
+        temp_x += region[i].first;
+        temp_y += region[i].second;        
     }
+    
+    sort(region.begin(), region.end(), comp);
 
-    temp_x/=weight_sum;
-    temp_y/=weight_sum;
-
-    printf("%lf ------- %lf\n", temp_x, temp_y);
-    vector<pair<int, int> > cands;
-
-    for(int i=0;i<1;++i)
-        for (int j=0;j<1;++j)
-        {
-            int intx = (i%2==0)?floor(temp_x):ceil(temp_x);
-            int inty = (j%2==0)?floor(temp_y):ceil(temp_y);
-            cands.push_back(pair<int, int>(intx, inty));
-        }
-
-    vector<pair<int, int> > res;
-
-    for(int i=0;i<cands.size();++i)
+    double temp_dis = 1000000000000;
+    for(int i=0;i<region.size();++i)
     {
-        int fit_x, fit_y;
-        long long dd = 100000000000000L;;
-        for(int j=0;j<region.size();++j)
+        double in_x = (temp_x - region[i].first)/(region.size()-1);
+        double in_y = (temp_y - region[i].second)/(region.size()-1);
+        double in_dis = tinyDis(in_x, in_y, region[i].first, region[i].second);
+        printf("distance is %lf\n", in_dis);
+        if(in_dis - temp_dis < 0.00000001)
         {
-            for(int l=region[j][0];l<=region[j][2];++l)
-            {
-                for(int m=region[j][1];m<=region[j][3];++m)
-                {
-                    long long temp = calculateDis(l,m,cands[i].first, cands[i].second);
-                    if(temp < dd)
-                    {
-                        dd = temp;
-                        fit_x = l;
-                        fit_y = m;
-                    }
-                }
-            }
+            temp_dis = in_dis;
+            x = region[i].first;
+            y = region[i].second;
         }
-        res.push_back(pair<int, int>(fit_x, fit_y));        
     }
 
-    weight_sum = 1000000000000L;
-    for(int i=0;i<res.size();++i)
-    {
-        long long temp_sum = calSum(region, res[i].first, res[i].second);
-        if(temp_sum < weight_sum){
-            weight_sum = temp_sum;
-            x = res[i].first;
-            y = res[i].second;
-        }
-    }
+    dis = calSum(region, x, y);
 }
 
 int main()
@@ -97,7 +75,8 @@ int main()
     int T, B;
     int x, y;
     long long dis;
-    vector<vector<int> > region;
+    vector<pnt > region;
+    vector<int> tmp = vector<int>(4);
     scanf("%d", &T);
 
     for(int ic=1;ic<=T;++ic)
@@ -106,10 +85,12 @@ int main()
         region.clear();
         for(int i=0;i<B;++i)
         {
-            vector<int> tmp = vector<int>(4);
+            
             for(int j=0;j<4;++j)
                 scanf("%d", &tmp[j]);
-            region.push_back(tmp);            
+            for(int l=tmp[0];l<=tmp[2];++l)
+                for(int m=tmp[1];m<=tmp[3];++m)
+                    region.push_back(pnt(l, m));
         }
         findPosition(region, x, y, dis);
         printf("Case #%d: %d %d %lld\n", ic, x, y, dis);
