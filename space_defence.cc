ea30c4bb3aa499a0d97b3a1ff653cf215090fd2d
washingtonzju <zhongkai.mi@gmail.com>
2014-02-26 22:33:17 +0800

    Create space_defence.cc

---
+++ space_defence.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<utility>
+using namespace std;
+
+
+int main()
+{
+    int T, N, M, S;
+    cin >> T;
+    for(int i=1;i<T+1;++i)
+    {
+        cin >> N;
+        
+    }
+    return 0;
+}
