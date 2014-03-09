d3d25f4575d6f83b3f699f8cf88874df1429a2d7
washingtonzju <zhongkai.mi@gmail.com>
2014-03-09 22:41:53 +0800

    Update 843_crypt_killer_opt.cc

--- 843_crypt_killer_opt.cc
+++ 843_crypt_killer_opt.cc
@@ -4,13 +4,13 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <set>
 using namespace std;
 
-typedef map<char, char> table;
+typedef string table;
 typedef map<string, vector<string> > dictionary;
 
 bool flg = false;
-
 string get_type(string word)
 {
     char tt = 'a';
@@ -28,24 +28,30 @@ void decode(vector<string> &wds, table &tb, dictionary &dic, int idx)
 {
     // return conditions
     if(flg==true) return;
-    if(tb.size() == 26 || idx == wds.size()) {
+    for(int i=0;i<26;++i)
+        for(int j=i+1;j<26;++j)
+            if(tb[i]==tb[j] && tb[i]!='*')
+                return;
+    if(idx == wds.size()) {        
         flg = true;
         return;
-    }
+    }    
     string type = get_type(wds[idx]);
     vector<string> &cands = dic[type];
     int len = wds[idx].size();
     for(int i=0;i<cands.size();++i) {
         int k=0;
-        table tmp(tb.begin(), tb.end());        
+        table tmp = tb;
         for(k=0;k<len;++k)
-            if(tb.find(wds[idx][k])!=tb.end() && tb[wds[idx][k]]!=cands[i][k]) break;
-            else tb[wds[idx][k]] = cands[i][k];
-        if(k==len) {
-            decode(wds, tb, dic, idx+1);
-            if(flg==true) return;
+            if(tmp[wds[idx][k]-'a']!='*' && tmp[wds[idx][k]-'a']!=cands[i][k]) break;
+            else tmp[wds[idx][k]-'a'] = cands[i][k];        
+        if(k==len) {            
+            decode(wds, tmp, dic, idx+1);
+            if(flg==true) {
+                tb = tmp;
+                return;
+            }
         }
-        tb=table(tmp.begin(), tmp.end());
     }
 }
 
@@ -57,29 +63,39 @@ int main()
     
     cin >> N;
     // build the dictionary: category with word length.
+    set<string> dup;
     for(int i=0; i<N; ++i)
     {
         cin >> word;
+        
+        if(dup.find(word)!=dup.end()) continue;
         string type = get_type(word);
         if(dic.find(type)==dic.end()) dic[type] = vector<string>();
         dic[type].push_back(word);
+        dup.insert(word);
     }
 
     string line;
+    getline(cin, line);
     while(getline(cin, line)){
         //turn line into words
+        if(line.length()==0) {cout << endl; continue;}
         std::stringstream ss(line);
         vector<string> wds;
+        set<string> uniq;
         while(ss >> word) {
+            if(uniq.find(word)!=uniq.end())
+                continue;
+            uniq.insert(word);
             wds.push_back(word);
         }
-        table tb;
+        table tb = "**************************";
         flg = false;
 
         decode(wds, tb, dic, 0);
         if(flg==true) {
             for(int i=0;i<line.size();++i)
-                if(tb.find(line[i])!=tb.end()) line[i] = tb[line[i]];
+                if(line[i]!=' ') line[i] = tb[line[i]-'a'];
         }
         else {
             for(int i=0;i<line.size();++i)
